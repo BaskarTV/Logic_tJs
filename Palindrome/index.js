670997ee69161d0cff42d9063a97adb1a46f00dd
baskar thangavelu <baskar.tece@gmail>
2023-08-12 18:24:50 +0530

    changes in the comparision

--- Palindrome/index.js
+++ Palindrome/index.js
@@ -4,9 +4,10 @@ function checkPalindrome(){
    const result = document.querySelector('#result');
    const reverseString = string.split('').reverse().join('');
 //    console.log(reverseString)
-   if (string === reverseString){
+   if (string !== reverseString){
       result.innerHTML = `It is a Palindrome`   
 }else{
       result.innerHTML = `It is not a Palindrome`
 }
 }
+
