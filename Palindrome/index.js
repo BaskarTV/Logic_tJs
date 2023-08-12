b8eb8939d0c70a75ed6bc4a052a7c6c6bbe7e311
baskar thangavelu <baskar.tece@gmail>
2023-08-12 12:47:56 +0530

    Palindrom_Checker

---
+++ Palindrome/index.js
@@ -0,0 +1,12 @@
+function checkPalindrome(){
+   const string = document.querySelector('#input').value.replace(/\s/g,'').toLowerCase();
+//    console.log(string) 
+   const result = document.querySelector('#result');
+   const reverseString = string.split('').reverse().join('');
+//    console.log(reverseString)
+   if (string === reverseString){
+      result.innerHTML = `It is a Palindrome`   
+}else{
+      result.innerHTML = `It is not a Palindrome`
+}
+}
