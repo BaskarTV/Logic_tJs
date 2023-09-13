4e6b08de486baff1cc3ced6063fe92d486322fb1
baskar thangavelu <baskar.tece@gmail>
2023-09-13 09:59:50 +0530

    Exercise added date & time

--- Digital_Clock/script.js
+++ Digital_Clock/script.js
@@ -15,11 +15,32 @@ function displayTime(){
     document.querySelector('#mins').innerHTML = mins
     document.querySelector('#seconds').innerHTML =sec
 
+    /* Adding date / month / year and day */
+     let date = dateTime.getDate();
+     let month = dateTime.getMonth() + 1;
+     let year = dateTime.getFullYear()
+     let weekDay =  getWeekDay(dateTime.getDay())
+     console.log(day)
+
+     document.querySelector('#date').innerHTML = `${padZero(date)}/${padZero(month)}/${year}`
+     document.querySelector('#day').innerHTML = `- ${weekDay}`   
+
+
 }
+
+/*PadZero function for Adding '0' to the begining in the time so the time will display in two digits */
 
 function padZero(num){
     return num < 10 ? '0' + num : num
 }
 
+/* Adding weekDays */
+
+function getWeekDay(day) {
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    return daysOfWeek[day];
+}
+
+/* Interval function for calling the displayTime function every 1000 milliseconds */
 
 setInterval(displayTime,1000)
