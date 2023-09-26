d25bfebdaf462486b33c159875198f00aaa01258
baskar thangavelu <baskar.tece@gmail>
2023-09-26 11:52:13 +0530

    added fetch call with async and try catch method

--- WeatherAPP/weather.js
+++ WeatherAPP/weather.js
@@ -18,7 +18,7 @@ function convertion(kel){
 
 // adding eventListner to btn for the Apicall using fetch method
 
-btn.addEventListener('click', () => {
+/* btn.addEventListener('click', () => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal.value}&appid=${apiKey}`)
     .then(res => res.json())
     .then(data => {
@@ -32,7 +32,42 @@ btn.addEventListener('click', () => {
         wind.innerHTML=`Wind speed: <span>${windSpd}</span>`
            
     })
-  .catch(err => alert('You entered wrong city name'))
+  .catch(err => alert('You entered wrong city name',err))
 
 })
+ */
 
+
+// converting fetch with async await with try catch method
+
+btn.addEventListener('click', async ()=> {
+    try{ 
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal.value}&appid=${apiKey}`);
+
+        if(response.ok){
+            const data = await response.json();
+            const cityName = data.name
+            const description= data.weather['0']['description'];
+            const temperature=data.main['temp']
+            const windSpd = data.wind['speed']
+            city.innerHTML = `Weather of <span>${cityName}</span>`
+            temp.innerHTML = `Temperature: <span>${convertion(temperature)}</span>`
+            descp.innerHTML=`Sky Conditions: <span>${description}</span>`
+            wind.innerHTML=`Wind speed: <span>${windSpd}</span>`
+            
+        }else{
+            // Handle error when response is not OK (eg... 404)
+            city.innerHTML = 'City not found';
+            temp.innerHTML = '';
+            descp.innerHTML = '';
+            wind.innerHTML = '';
+        }
+    }
+    catch (error){
+        console.error('Error', error);
+        city.innerHTML = 'An error occured';
+        temp.innerHTML = "";
+        descp.innerHTML = "";
+        wind.innerHTML = "";
+    }
+})
